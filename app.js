49c7d24455ae6f10200629028863c7c2f2518e6a
Daniel Lundin <d-dog@d-dog.se>
2021-12-05 17:48:56 +0100

    upgrade packages

--- app.js
+++ app.js
@@ -1,78 +1,54 @@
-var fs = require('fs');
-var pf = require('./playfield');
-var g = require('./game.js');
-var names = require('./names.js');
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+var g = require("./game.js");
+var names = require("./names.js");
 
-var app = require('http').createServer(handler);
-var io = require('socket.io').listen(app);
-io.set('log level', 1)
-app.listen(process.env.PORT || 8080);
+const app = express();
+const server = http.createServer(app);
+app.use(express.static("static"));
+const io = new Server(server);
 
-static_url_map = {
-    '/': ['index.html','text/html'],
-    '/static/js/drawing.js': ['/static/js/drawing.js', 'application/javascript'],
-    '/static/js/nodemock.js': ['/static/js/nodemock.js', 'application/javascript'],
-    '/static/js/shapes.js': ['/shapes.js', 'application/javascript'],
-    '/static/js/playfield.js': ['/playfield.js', 'application/javascript'],
-}
-
-function serve_static(res, url) {
-    if(url in static_url_map) {
-        var match = static_url_map[url];
-        path = match[0];
-        content_type = match[1];
-        fs.readFile(__dirname + "/" + path,
-            function (err, data) {
-                if(err) {
-                    res.writeHead(500);
-                    console.log(err);
-                    return res.end("Error loading static file" + path);
-                }
-                res.writeHead(200, {'Content-Type': content_type});
-                res.end(data);
-            }
-        );
-    }
-}
-
-function handler(req, res) {
-    serve_static(res, req.url);
-}
-
-
-io.sockets.on('connection', function (socket) {
-    socket.name = names.shuffle_name();
-    console.log('registered player ' + socket.name);
-    socket.emit('registered', socket.name);
-    g.GameMgr.find_or_create_game(socket);
-
-    var game = g.GameMgr.game_for(socket);
-    socket.on('message', function (data) {
-        switch(data) {
-            case 'left':
-                game.move_left(socket);
-                break;
-            case 'right':
-                game.move_right(socket);
-                break;
-            case 'rotate':
-                game.rotate(socket);
-                break;
-            case 'down':
-                game.move_down(socket);
-                break;
-            case 'start_game':
-                game.start_game(socket);
-                break;
-            case 'free_fall':
-                game.free_fall(socket);
-                break;
-        }
-    });
-
-    socket.on('disconnect', function (data) {
-        console.log('disconnect');
-        game.remove_player(socket);
-    });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+io.on("connection", (socket) => {
+  socket.name = names.shuffle_name();
+  console.log("registered player " + socket.name);
+  socket.emit("registered", socket.name);
+  g.GameMgr.find_or_create_game(socket);
+
+  var game = g.GameMgr.game_for(socket);
+  socket.on("message", function (data) {
+    switch (data) {
+      case "left":
+        game.move_left(socket);
+        break;
+      case "right":
+        game.move_right(socket);
+        break;
+      case "rotate":
+        game.rotate(socket);
+        break;
+      case "down":
+        game.move_down(socket);
+        break;
+      case "start_game":
+        game.start_game(socket);
+        break;
+      case "free_fall":
+        game.free_fall(socket);
+        break;
+    }
+  });
+
+  socket.on("disconnect", function () {
+    console.log("disconnect");
+    game.remove_player(socket);
+  });
+});
---
+++ static/js/shapes.js
@@ -0,0 +1,108 @@
+
+function box() {
+    return [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ]
+}
+
+function pyramid() {
+    return [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+}
+
+function I() {
+    return [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0]
+    ]
+}
+
+function L() {
+    return [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+    ]
+}
+
+function zed() {
+    return [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+}
+
+factories = [box, pyramid, I, L, zed];
+
+function rotate(shape) {
+
+    var new_shape = Array(shape.length);
+    for(var i=0;i<shape.length; ++i) {
+        new_shape[i] = Array(shape.length);
+    }
+
+    for(var i=0;i<shape.length; ++i) {
+        for(var j=0;j<shape[i].length; ++j) {
+            new_shape[i][j] = shape[shape.length-j-1][i];
+        }
+    }
+    // A 4x4 rotation does this:
+    /*
+    new_shape[0][0] = shape[3][0];
+    new_shape[0][1] = shape[2][0];
+    new_shape[0][2] = shape[1][0];
+    new_shape[0][3] = shape[0][0];
+
+    new_shape[1][0] = shape[3][1];
+    new_shape[1][1] = shape[2][1];
+    new_shape[1][2] = shape[1][1];
+    new_shape[1][3] = shape[0][1];
+
+    new_shape[2][0] = shape[3][2];
+    new_shape[2][1] = shape[2][2];
+    new_shape[2][2] = shape[1][2];
+    new_shape[2][3] = shape[0][2];
+
+    new_shape[3][0] = shape[3][3];
+    new_shape[3][1] = shape[2][3];
+    new_shape[3][2] = shape[1][3];
+    new_shape[3][3] = shape[0][3];
+    */
+
+    return new_shape;
+}
+
+function randomize_color(shape) {
+    var color = Math.floor(Math.random()*3)+1;
+    for(var i=0;i<shape.length; ++i) {
+        for(var j=0;j<shape[i].length; ++j) {
+            if(shape[i][j]) {
+                shape[i][j] = color;
+            }
+        }
+    }
+}
+
+exports.factories = factories;
+exports.rotate = rotate;
+exports.randomize_color = randomize_color;
+
+exports._box = box;
+exports._pyramid = pyramid;
+exports._I = I;
+exports._L = L;
+exports._zed = zed;
